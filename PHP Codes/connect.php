b7baa309e477908a8a654841249527e9470c337a
SharukhFardin <sharukhfardin1234@gmail.com>
2020-09-15 16:37:19 +0600

    Adding some php codes :D

---
+++ PHP Codes/connect.php
@@ -0,0 +1,14 @@
+<?php
+	$db_server = "localhost";
+    $db_user   = "root";
+    $db_pass   = "";
+    $db_name   = "final-project";
+
+	//Connecting to the database
+	$conn = mysqli_connect($db_server,$db_user,$db_pass,$db_name);
+	echo "connected to the database";
+    if(!$conn){
+    	die("Connection failed! :".mysqli_connect_error());
+  	}
+		
+?>
---
+++ PHP Codes/insert_doner.php
@@ -0,0 +1,33 @@
+<?php
+	//import connect.php
+	require 'connect.php';
+
+	if(isset($_POST['first_name'])){
+		$first_name = $_POST['first_name'];
+		$last_name = $_POST['last_name'];
+		$phone_number = $_POST['phone_number'];
+		$email = $_POST['email'];
+		$sex = $_POST['sex'];
+		$User_name = $_POST['User_name'];
+		$password = $_POST['password'];
+		$birth_date = $_POST['birth_date'];
+		$blood_group = $_POST['blood_group'];
+		$last_donated = $_POST['last_donated'];
+		$address = $_POST['address'];
+		$corona_recovery = $_POST['corona_recovery'];
+
+
+	//inserting data
+		$is_inserted = mysqli_query($conn,"
+			INSERT INTO blood_donor(User_name,password,first_name,last_name,birth_date,sex,blood_group,phone_number,email,address, corona_recovery, last_donated)
+			VALUES('$User_name','$password','$first_name','$last_name','$birth_date','$sex','$blood_group','$phone_number','$email','$address','$corona_recovery','$last_donated')");
+
+
+		}
+		
+		if($is_inserted){
+  			echo "Inserted successfully";
+		}else{
+  			echo "Opps error!";
+		}
+?>
