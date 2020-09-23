71d9d5487febe5398dbbecf289745f6834076d88
SharukhFardin <sharukhfardin1234@gmail.com>
2020-09-23 06:29:10 +0600

    commit

--- PHP Codes/seeker_login.php
+++ PHP Codes/seeker_login.php
@@ -1,22 +1,23 @@
 <?php
+	ini_set('display_errors','1');
 	//Initialize the session
 	session_start();
 
 	//import connect.php
 	require 'connect.php';
 
-	if(isset($_POST['User_name'])){
-		$User_name = $_POST['User_name'];
+	if(isset($_POST['user_name'])){
+		$user_name = $_POST['user_name'];
 		$password = $_POST['password'];
 	}
-		$stmt = $conn->prepare("SELECT * FROM blood_donor WHERE User_name=?");
-		$stmt->bind_param("s", $User_name);
+		$stmt = $conn->prepare("SELECT * FROM blood_seeker WHERE user_name=?");
+		$stmt->bind_param("s", $user_name);
 		$stmt->execute();
 		$stmt_result = $stmt->get_result();
 		if($stmt_result->num_rows >0){
 			$data = $stmt_result->fetch_assoc();
 			if($data['password'] === $password){
-				header("location: donor_welcome.html");
+				header("location: seeker_welcome.html");
 			}
 			else{
 				echo "<h2>Invalid username or password</h2>";
